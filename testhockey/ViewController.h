b1c3173c346a55db54f00fef9292ebad0c6d699e
pudgeli <pudgeli@tencent.com>
2016-08-28 19:48:48 +0800

    Initial Commit

---
+++ testhockey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testhockey
+//
+//  Created by dily fu on 16/8/28.
+//  Copyright © 2016年 pudgeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testhockey/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testhockey
+//
+//  Created by dily fu on 16/8/28.
+//  Copyright © 2016年 pudgeli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
